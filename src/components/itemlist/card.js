d42f054beb2c220c94fd7a6a02f08d5ee5d3e28f
Nishant nayak <nayaknishant656@gmail.com>
2023-06-27 23:23:20 +0530

    card-changes

--- src/components/itemlist/card.js
+++ src/components/itemlist/card.js
@@ -6,24 +6,22 @@ import Tshirt from "../body/img/background-images/crew_front.png";
 const card = ({ item, handleClick }) => {
   const { id, title, author, price, image, rating } = item;
   const Id = [rating];
-  const mystyle = {
-    height: "190px",
-    width: "145px",
-  };
+  // const mystyle = {
+  //   height: "190px",
+  //   width: "145px",
+  // };
   return (
        
          <div className="cards">
 <NavLink to={`/cards/${item.id}`} class="link-cart">
       <div className="image_box">
-        <img src={image} alt="Image" style={mystyle} />
+        <img src={image} alt="Image"  />
       </div>
       <div className="details">
         {/* <p>{id}</p> */}
         <p style={{ fontSize: "15px", fontWeight: 600 }}>{title.slice(0, 25)}</p>
         {/* <p>{author}</p> */}
         <p style={{fontsize: "12px", color: "black" }}>Price - {price}Rs</p>  
-         
-                
                           <span>{rating.rate}<FiStar/></span> ||
 <span>{rating.count}</span> 
                   
